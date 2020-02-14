4fb5d07e9fb622bb9003028b655da773e1ceccaf
Emiliano Larrea <emiliano18796@gmail.com>
2020-02-14 12:27:55 -0300

    Http exception

--- src/Utility/Http/Http.php
+++ src/Utility/Http/Http.php
@@ -3,21 +3,25 @@ namespace App\Utility\Http;
 
 use Cake\Http\Client;
 use Cake\Core\Configure;
-
-
+use Cake\Core\Exception\Exception;
+use Cake\Log\Log;
 class Http{
     
     public static function http_connection($method, $url, $parameters = [], $headers = [], $type = "json"){
-
         $http = new Client();
-
-        if($method == "GET"){
-            $response = $http->get($url, $parameters,['headers' => $headers, 'type' => $type]);         
+        try {
+            if($method == "GET"){
+                $response = $http->get($url, $parameters,['headers' => $headers, 'type' => $type]);         
+            }
+            if($method == "POST"){
+                $response = $http->post($url, $parameters,['headers' => $headers, 'type' => $type]);
+            }
+            return $response;
+        } catch (Exception $e) {
+            Log::write('error', 'COULD NOT RESOLVE HOST: '.$url.', API ERROR');
+            echo json_encode(['error' => "503 Service Unavailable"]);
+            header("HTTP/1.1 503 Service Unavailable");
+            exit;
         }
-        if($method == "POST"){
-            $response = $http->post($url, $parameters,['headers' => $headers, 'type' => $type]);
-        }
-        
-        return $response;
     }
 }
